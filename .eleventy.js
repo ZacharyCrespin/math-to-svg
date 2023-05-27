a722cb3ea01f68727e070f35f8aef71b00ef473e
Zachary Crespin <Zachary.lights@gmail.com>
2023-05-27 09:01:14 -0700

    fix input on mobile

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,9 @@ const filesMinifier = require("@sherby/eleventy-plugin-files-minifier");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(filesMinifier);
   
+  eleventyConfig.addWatchTarget("./src/style.css");
+  eleventyConfig.addWatchTarget("./src/script.js");
+
   return {
     dir: {
       input: "src",
